73e6e50fddf865fa05955362450a1a22ec2539a9
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2015-12-03 13:32:59 -0700

    Added pictures to the files for the slider, accessible through jQuery.

--- index.php
+++ index.php
@@ -2,47 +2,25 @@
 <html>
 <head>
     <title>GamePlan</title>
-    <style>
-        #TopSpace{
-            height: 1px;
-            width: 100%;
-        }
-        #Header{
-            margin-top: 14px;
-            width: 100%;
-            height: 80px;
-            background-color: #46d146;
-            fill-opacity: 0.75;
-        }
-        #Navigation{
-            float: left;
-            width: 180px;
-            height: 675px;
-            background-color: #D21620;
-        }
-        #LoginArea {
-            float:right;
-            width: 180px;
-            height: 80px;
-            background-color: #1D279B;
-        }
-    </style>
+    <link href="Styles_Index.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<div id="TopSpace">
-    <!-- Nothing goes here this is just here to make is so I can have a margin on the <div id="Header"> -->
-</div>
-<div id="Header">
-    <img src="GamePlan_logo_2.png" style="height: 80px; width: 116px; margin-left: 20px;" alt="Logo">
-    <!-- Main Header should go here -->
-    <div id="LoginArea" style="color: white;">
-        <!-- This is where the Login and Username and SignUp with a new account will happen -->
-        <p style="margin-top: 0; margin-bottom: 0;">Login</p>
-        <p style="margin-top: 0; margin-bottom: 0;">Sign Up</p>
+<div id="Main">
+    <div id="TopSpace">
+        <!-- Nothing goes here this is just here to make is so I can have a margin on the <div id="Header"> -->
     </div>
-</div>
-<div id="Navigation">
-    <!-- everything navigation goes here (other than signing up and logging out and creating new accounts) -->
+    <div id="Header">
+        <img src="GamePlan_logo_2.png" style="height: 80px; width: 116px; margin-left: 20px;" alt="Logo">
+        <!-- Main Header should go here -->
+        <div id="LoginArea" style="color: white;">
+            <!-- This is where the Login and Username and SignUp with a new account will happen -->
+            <p style="margin-top: 0; margin-bottom: 0;">Login</p>
+            <p style="margin-top: 0; margin-bottom: 0;">Sign Up</p>
+        </div>
+    </div>
+    <div id="Navigation">
+        <!-- everything navigation goes here (other than signing up and logging out and creating new accounts) -->
+    </div>
 </div>
 </body>
 </html>
